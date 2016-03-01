6119b67a97d27b632b89af4760a88ab2b1d19e92
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-03-01 10:10:29 +0900

    yahooのスパイダー出来た

--- anotoki/yahoo_spider/yahoo_spider/spiders/top_news_spider.py
+++ anotoki/yahoo_spider/yahoo_spider/spiders/top_news_spider.py
@@ -8,11 +8,19 @@ class TopNewsSpider(scrapy.Spider):
     allowed_domains = ["news.yahoo.co.jp"]
 
     def __init__(self, prefecture=None, *args, **kwargs):
-        self.start_urls = [
-            'http://news.yahoo.co.jp/',
-            'http://news.yahoo.co.jp/hl?c=bus'
-        ]
+        self.start_urls = ['http://news.yahoo.co.jp/topics']
 
     def parse(self, response):
-        for li in response.xpath('//ul[@class="topics"]/li[not(@class)]'):
-            print li.xpath('.//a/text()').extract_first()
+        for li in response.xpath('//ul[@class="fl" or @class="fr"]/li[not(@class)]'):
+            yield scrapy.Request(li.xpath('.//a/@href').extract_first(), callback=self.parse_detail)
+            break
+
+    def parse_detail(self, response):
+        item = {}
+        item['category'] = response.xpath('//ul[@id="gnSec"]/li[@class="current"]/a/text()').extract_first()
+        item['posted_at'] = response.xpath('//div[@class="topicsName"]/span[@class="date"]/text()').extract_first()
+        item['title'] = response.xpath('//h1/text()').extract_first()
+        item['topic_link'] = response.url
+        item['text'] = response.xpath('//p[@class="hbody"]/text()').extract_first()
+        item['detail_link'] = response.xpath('//a[@class="newsLink"]/@href').extract_first()
+        return item
