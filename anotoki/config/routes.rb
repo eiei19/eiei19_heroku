921b54e5479f5f67997826519b12a93834f9e60c
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-03-08 18:21:23 +0900

    sitemap redirect

--- anotoki/config/routes.rb
+++ anotoki/config/routes.rb
@@ -2,4 +2,5 @@ Anotoki::Engine.routes.draw do
   root 'news#index'
   get '/:yyyy/:mm/:dd', to: 'news#date', constraints: {yyyy: /\d{4}/, mm: /\d{2}/, dd: /\d{2}/}
   get '/:yyyy/:mm/:dd/:keyword', to: 'news#keyword', constraints: {yyyy: /\d{4}/, mm: /\d{2}/, dd: /\d{2}/}
+  get 'sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/anotoki/anotoki_sitemaps/sitemap.xml.gz")
 end
