11a3d88d618bb818cc2d24378ec78109b87d52c9
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-04-28 14:41:21 +0900

    add fonts

--- app/controllers/api/screenshot_controller.rb
+++ app/controllers/api/screenshot_controller.rb
@@ -1,23 +1,35 @@
 module Api
   class ScreenshotController < ApplicationController
     def index
-      s3 = Aws::S3::Client.new(
+      if Rails.env.production?
+        if params[:api_key].blank? || params[:api_key] != ENV["SCREENSHOT_API_KEY"]
+          render json: {error: "unauthorized"}
+          return
+        end
+      end
+      if params[:url].blank? || !(params[:url] =~ URI::regexp)
+        render json: {error: "invalid url"}
+        return
+      end
+      width = params[:width] || 1200
+      height = params[:height] || 900
+      setting = {
         access_key_id: ENV["AWS_ACCESS_KEY_ID"],
         secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
         region: "ap-northeast-1"
-      )
-
-
+      }
+      s3 = Aws::S3::Client.new(setting)
       ws = Webshot::Screenshot.instance
-      shot = ws.capture "http://www.google.com/", "tmp/google.png", width: 1200, height: 900, quality: 90
-
+      shot = ws.capture params[:url], "tmp/tmp.png", width: width, height: height, quality: 100
+      file_name = "#{SecureRandom.hex(16)}.png"
       s3.put_object(
         bucket: "easyscreenshots",
         body: shot.to_blob,
-        key: "test.png"
+        key: file_name
       )
-
-      render json: []
+      s3 = Aws::S3::Resource.new(setting)
+      obj = s3.bucket("easyscreenshots").object(file_name)
+      render json: {url: obj.public_url}
     end
   end
 end
