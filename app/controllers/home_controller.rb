d9d0f9c03211d9e6b4ebeca4060fc5b77503b909
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-04-28 12:05:30 +0900

    path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     ws = Webshot::Screenshot.instance
-    ws.capture "http://www.google.com/", "google.png", width: 1200, height: 900, quality: 90
+    ws.capture "http://www.google.com/", "tmp/google.png", width: 1200, height: 900, quality: 90
   end
 end
